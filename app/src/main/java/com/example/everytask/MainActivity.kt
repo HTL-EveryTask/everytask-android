75132780bb74ad0fc9efb315d147444473212c98
myBernhard <92299433+myBernhard@users.noreply.github.com>
2022-10-29 13:55:16 +0200

    finished login and register frontend validation

--- app/src/main/java/com/example/everytask/MainActivity.kt
+++ app/src/main/java/com/example/everytask/MainActivity.kt
@@ -13,6 +13,9 @@ import com.example.everytask.fragments.ConnectionsFragment
 import com.example.everytask.fragments.GroupsFragment
 import com.example.everytask.fragments.HomeFragment
 import com.example.everytask.fragments.SettingsFragment
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mainBinding: ActivityMainBinding
@@ -40,56 +43,117 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun login(view: View) {
-        setContentView(mainBinding.root)
-        replaceFragment(homeFragment)
-        mainBinding.bottomNavigation.setOnItemSelectedListener {
-            when (it.itemId) {
-                R.id.home -> replaceFragment(homeFragment)
-                R.id.groups -> replaceFragment(groupsFragment)
-                R.id.connections -> replaceFragment(connectionsFragment)
-                R.id.settings -> replaceFragment(settingsFragment)
+        val validEmail = validEmail(loginBinding.etEmail.text.toString()) == null
+
+        if (validEmail) {
+            setContentView(mainBinding.root)
+            replaceFragment(homeFragment)
+            mainBinding.bottomNavigation.setOnItemSelectedListener {
+                when (it.itemId) {
+                    R.id.home -> replaceFragment(homeFragment)
+                    R.id.groups -> replaceFragment(groupsFragment)
+                    R.id.connections -> replaceFragment(connectionsFragment)
+                    R.id.settings -> replaceFragment(settingsFragment)
+                }
+                true
             }
-            true
         }
     }
 
     fun register(view: View) {
-        mainBinding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(mainBinding.root)
-        replaceFragment(homeFragment)
-        mainBinding.bottomNavigation.setOnItemSelectedListener {
-            when (it.itemId) {
-                R.id.home -> replaceFragment(homeFragment)
-                R.id.groups -> replaceFragment(groupsFragment)
-                R.id.connections -> replaceFragment(connectionsFragment)
-                R.id.settings -> replaceFragment(settingsFragment)
+        val validEmail = validEmail(registerBinding.etEmail.text.toString()) == null
+        val validPassword = validPassword(registerBinding.etPassword.text.toString()) == null
+        val validConfirmPassword =
+            registerBinding.etPassword.text.toString() == registerBinding.etConfirmPassword.text.toString()
+
+        if(validEmail && validPassword && validConfirmPassword) {
+            setContentView(mainBinding.root)
+            replaceFragment(homeFragment)
+            mainBinding.bottomNavigation.setOnItemSelectedListener {
+                when (it.itemId) {
+                    R.id.home -> replaceFragment(homeFragment)
+                    R.id.groups -> replaceFragment(groupsFragment)
+                    R.id.connections -> replaceFragment(connectionsFragment)
+                    R.id.settings -> replaceFragment(settingsFragment)
+                }
+                true
             }
-            true
         }
     }
 
     fun toLogin(view: View) {
         setContentView(loginBinding.root)
-        emailFocusListener()
+        emailFocusListener(loginBinding.etEmail, loginBinding.tilEmailContainer)
+        // TODO add password focus listener? also add password validation in login()
     }
 
     fun toRegister(view: View) {
         setContentView(registerBinding.root)
+        emailFocusListener(registerBinding.etEmail, registerBinding.tilEmailContainer)
+        passwordFocusListener(
+            registerBinding.etPassword,
+            registerBinding.tilPasswordContainer,
+            registerBinding.tilConfirmPasswordContainer
+        )
+        confirmPasswordFocusListener()
     }
 
-    private fun emailFocusListener() {
-        loginBinding.etEmail.setOnFocusChangeListener { _, focused ->
+    private fun confirmPasswordFocusListener() {
+        // TODO : ERROR MAYBE?? statt helperText
+        registerBinding.etConfirmPassword.setOnFocusChangeListener { _, focused ->
             if (!focused) {
-                loginBinding.tilEmailContainer.helperText = validEmail()
+                if (registerBinding.etPassword.text.toString() != registerBinding.etConfirmPassword.text.toString()) {
+                    registerBinding.tilConfirmPasswordContainer.helperText =
+                        "Passwords do not match"
+                } else {
+                    registerBinding.tilConfirmPasswordContainer.helperText = null
+                }
             }
         }
     }
 
-    private fun validEmail(): String? {
-        val email = loginBinding.etEmail.text.toString()
-        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-            return "Invalid Email Address"
+    private fun emailFocusListener(etEmail: TextInputEditText, tilEmailContainer: TextInputLayout) {
+        // TODO : ERROR MAYBE?? statt helperText
+        etEmail.setOnFocusChangeListener { _, focused ->
+            if (!focused) {
+                tilEmailContainer.helperText = validEmail(etEmail.text.toString())
+            }
         }
-        return null
+    }
+
+    private fun passwordFocusListener(
+        etPassword: TextInputEditText,
+        tilPasswordContainer: TextInputLayout,
+        tilConfirmPasswordContainer: TextInputLayout? = null
+    ) {
+        // TODO : ERROR MAYBE?? statt helperText
+        etPassword.setOnFocusChangeListener { _, focused ->
+            if (!focused) {
+                tilPasswordContainer.helperText = validPassword(etPassword.text.toString())
+                    tilConfirmPasswordContainer?.helperText =
+                        "Passwords do not match"
+            }
+        }
+    }
+
+    private fun validPassword(password: String): String? {
+        return when {
+            password.isEmpty() -> "Password is required"
+            password.length < 8 -> "Password must be at least 8 characters"
+            //TODO Matching regex checken
+            !password.matches(".*[a-z].*".toRegex()) -> "Password must contain at least one lowercase letter"
+            !password.matches(".*[A-Z].*".toRegex()) -> "Password must contain at least one uppercase letter"
+            !password.matches(".*[0-9].*".toRegex()) -> "Password must contain at least one number"
+            !password.matches(".*[!@#\$%^&*()_+].*".toRegex()) -> "Password must contain at least one special character"
+            else -> null
+        }
+    }
+
+    private fun validEmail(email: String): String? {
+        return when {
+            email.isEmpty() -> "Email is required"
+            !Patterns.EMAIL_ADDRESS.matcher(email).matches() -> "Invalid Email Address"
+            else -> null
+        }
     }
 }
